e246c533794c36d8d4177508c03cf0450a5b9461
Roberto Moia <moiacr@gmail.com>
2013-12-09 10:31:47 -0600

    Maquetado del app

--- README.md
+++
@@ -1,4 +0,0 @@
-rodco-buy
-=========
-
-App para que los clientes puedan hacer pedidos
---
+++ index.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>RODCO Compras</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+	<div class="row">
+		<nav class="navbar col-lg-12" role="navigation">
+			<div class="navbar-header col-lg-4">
+  				<h3 class="" href="#">RODCO Compras</h3>
+  			</div>
+			<form class="navbar-form navbar-right form-inline col-lg-8" role="search">
+				<div class="form-group col-lg-6">
+					<label for="" class="col-lg-4 text-right"><span class="glyphicon glyphicon-earphone"></span> Tel</label>
+					<div class="col-lg-8">
+						<input type="text" class="form-control input-lg" placeholder="0000-00-00">
+					</div>
+				</div>
+				<div class="form-group col-lg-6">
+					<label for="" class="col-lg-4 text-right"><span class="glyphicon glyphicon-phone"></span> Pin</label>
+					<div class="col-lg-8">
+						<input type="text" class="form-control input-lg" placeholder="Ej: 4367">
+					</div>
+				</div>
+			</form>
+		</nav>
+	</div>
+
+	<div class="row">
+		<header class="col-lg-12 text-center">
+			<div class="row">
+				<h2>SELECCIONE LOS <span class="text-primary">PRODUCTOS</span> QUE DESEA.</h2>
+			</div>
+			<div class="row page-header">
+				<form class="form-inline col-lg-12" role="search">
+					<div class="form-group col-lg-4 col-lg-offset-2">
+						<label class="col-lg-3">Buscar</label>
+						<div class="col-lg-9">
+    						<input type="text" class="form-control input-lg" name="" placeholder="Nombre">
+    					</div>
+					</div>
+					<div class="form-group col-lg-4">
+						<label class="col-lg-3">Dpto</label>
+						<div class="btn-group col-lg-9">
+						  <button type="button" class="btn btn-default btn-lg btn-block dropdown-toggle" data-toggle="dropdown">
+						    TODOS <span class="caret"></span>
+						  </button>
+						  <ul class="dropdown-menu" role="menu">
+						   	<li><a href="#">Candados</a></li>
+						   	<li><a href="#">Metales</a></li>
+						   	<li><a href="#">Remate</a></li>
+						   	<li><a href="#">Cerraduras</a></li>
+						   	<li><a href="#">Ferretería</a></li>
+						   	<li><a href="#">Eléctrico</a></li>
+						   	<li><a href="#">Coflex</a></li>
+						   	<li><a href="#">Plywood</a></li>
+						  </ul>
+						</div>
+					</div>				
+				</form>
+			</div>
+		</header>
+	</div>	
+
+	<div class="row">
+		<section class="col-lg-10 col-lg-offset-1">
+
+			<div class="col-lg-7">
+
+				<div class="row">
+					<article class="product col-lg-12">
+
+						<div class="col-lg-12">
+							<a href="" class="btn btn-default btn-block">
+								<h4 class="col-lg-12 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>
+								<h4 class="col-lg-12"> 
+									<span class="pull-left">Precio:</span>
+									<span class="text-primary">Disponibles:67</span>
+									<span class="pull-right">Descuento:3%</span>
+								</h4>
+							</a>
+							<div class="progress">
+							  <div class="progress-bar progress-bar-primary"  role="progressbar" aria-valuenow="67" aria-valuemin="0" aria-valuemax="100" style="width: 67%">
+							    <span class="sr-only">67% Complete</span>
+							  </div>
+							</div>
+						</div>
+					</article><!-- Un Producto -->
+				</div>
+
+				
+
+				<div class="row">
+					<article class="product col-lg-12">
+
+						<div class="col-lg-12">
+							<a href="" class="btn btn-default btn-block">
+								<h4 class="col-lg-12 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>
+								<h4 class="col-lg-12"> 
+									<span class="pull-left">Precio:</span>
+									<span class="text-primary">Disponibles:90</span>
+									<span class="pull-right">Descuento:3%</span>
+								</h4>
+							</a>
+							<div class="progress">
+							  <div class="progress-bar progress-bar-primary"  role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+							    <span class="sr-only">90% Complete</span>
+							  </div>
+							</div>
+						</div>
+					</article><!-- Un Producto -->
+				</div>
+
+				
+
+				<div class="row">
+					<article class="product col-lg-12">
+
+						<div class="col-lg-12">
+							<a href="" class="btn btn-default btn-block">
+								<h4 class="col-lg-12 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>
+								<h4 class="col-lg-12"> 
+									<span class="pull-left">Precio:</span>
+									<span class="text-danger">Disponibles:2</span>
+									<span class="pull-right">Descuento:3%</span>
+								</h4>
+							</a>
+							<div class="progress">
+							  <div class="progress-bar progress-bar-danger"  role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%">
+							    <span class="sr-only">2% Complete</span>
+							  </div>
+							</div>
+						</div>
+					</article><!-- Un Producto -->
+				</div>
+				
+				
+
+				<div class="row">
+					<article class="product col-lg-12">
+
+						<div class="col-lg-12">
+							<a href="" class="btn btn-default btn-block">
+								<h4 class="col-lg-12 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>
+								<h4 class="col-lg-12"> 
+									<span class="pull-left">Precio:</span>
+									<span class="text-danger">Disponibles:13</span>
+									<span class="pull-right">Descuento:3%</span>
+								</h4>
+							</a>
+							<div class="progress">
+							  <div class="progress-bar progress-bar-danger"  role="progressbar" aria-valuenow="13" aria-valuemin="0" aria-valuemax="100" style="width: 13%">
+							    <span class="sr-only">13% Complete</span>
+							  </div>
+							</div>
+						</div>
+					</article><!-- Un Producto -->
+				</div>
+
+				
+
+				<div class="row">
+					<article class="product col-lg-12">
+						<div class="col-lg-2">
+							<div class="text-right">
+								<h4>Cant.</h4>
+							</div>
+							<form role="form">
+								<div class="form-group">
+								  <input type="text" class="form-control input-lg input-block text-center" value="17">
+								</div>
+							</form>
+						</div>
+						<div class="col-lg-10">
+							<a href="" class="btn btn-default btn-block active">
+								<h4 class="col-lg-12 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>
+								<h4 class="col-lg-12"> 
+									<span class="pull-left">Precio:</span>
+									<span class="text-primary">Disponibles:45</span>
+									<span class="pull-right">Descuento:3%</span>
+								</h4>
+							</a>
+							<div class="progress">
+							  <div class="progress-bar progress-bar-primary"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+							    <span class="sr-only">45% Complete</span>
+							  </div>
+							</div>
+						</div>
+					</article><!-- Un Producto -->
+				</div>
+
+			</div><!-- Lista de Productos -->
+
+			
+			<div class="col-lg-5">
+				<section class="added products">
+					<h3 class="text-primary">Productos Agregados <span class="pull-right">4</span><span class="glyphicon glyphicon-shopping-cart pull-right"></span></h3>
+					
+					<div class="row">
+						<label for="" class="col-lg-12">TOTAL</label>
+						<h4 class="col-lg-7">C: 3.085,00</h4>
+						<a href="su-orden.php" class="btn btn-success btn-lg col-lg-5">Confirmar <span class="glyphicon glyphicon-ok"></span></a>
+					</div>
+
+					
+
+					<div class="row">
+						<label for="" class="col-lg-3">Cant.</label>
+						<label for="" class="col-lg-9">Productos</label>
+					</div>
+
+					<div class="">
+
+						<div class="row">
+							<form role="form" class="col-lg-3">
+								<div class="form-group">
+								  <input type="text" class="form-control input-lg text-center" value="4">
+								</div>
+							</form>
+							<a href="" class="btn btn-default col-lg-9">
+								<h3 class="col-lg-2"><span class="glyphicon glyphicon-remove-circle text-danger"></span></h3>
+								<h4 class="col-lg-10 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>	
+							</a>
+						</div><!-- Un Producto Agregado -->
+						
+						<div class="row">
+							<form role="form" class="col-lg-3">
+								<div class="form-group">
+								  <input type="text" class="form-control input-lg text-center" value="4">
+								</div>
+							</form>
+							<a href="" class="btn btn-default col-lg-9">
+								<h3 class="col-lg-2"><span class="glyphicon glyphicon-remove-circle text-danger"></span></h3>
+								<h4 class="col-lg-10 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>	
+							</a>
+						</div><!-- Un Producto Agregado -->
+						
+						<div class="row">
+							<form role="form" class="col-lg-3">
+								<div class="form-group">
+								  <input type="text" class="form-control input-lg text-center" value="4">
+								</div>
+							</form>
+							<a href="" class="btn btn-default col-lg-9">
+								<h3 class="col-lg-2"><span class="glyphicon glyphicon-remove-circle text-danger"></span></h3>
+								<h4 class="col-lg-10 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>	
+							</a>
+						</div><!-- Un Producto Agregado -->
+						
+						<div class="row">
+							<form role="form" class="col-lg-3">
+								<div class="form-group">
+								  <input type="text" class="form-control input-lg text-center" value="4">
+								</div>
+							</form>
+							<a href="" class="btn btn-default col-lg-9">
+								<h3 class="col-lg-2"><span class="glyphicon glyphicon-remove-circle text-danger"></span></h3>
+								<h4 class="col-lg-10 text-left">CANDADOS YALE 11840 VISUAL BLISTER</h4>	
+							</a>
+						</div><!-- Un Producto Agregado -->
+
+					</div><!-- Lista de Productos Agregados -->
+				</section><!-- SideBar Productos Agregados -->
+			</div><!-- Columna -->
+		</section><!-- Contene	dor de Lista de Productos y Productos Agregados -->
+	</div>
+
+	<script src="_/js/bootstrap.js"></script>
+	<script src="_/js/myscript.js"></script>
+</body>
+</html>
